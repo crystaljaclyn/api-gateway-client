7f6a453ff9b78a7060cf61f30e2e63a9b6f08c44
tuannguyenminh040294 <37282910+tuannguyenminh040294@users.noreply.github.com>
2020-08-06 23:53:59 +0700

    First Commit

---
+++ src/main/java/com/arslan/apigatewayclient/ApiGatewayClientApplication.java
@@ -0,0 +1,13 @@
+package com.arslan.apigatewayclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arslan/apigatewayclient/ApiGatewayClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arslan.apigatewayclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
