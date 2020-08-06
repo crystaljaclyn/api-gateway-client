5893d309ccaaed0cebf0469c6780ebd48e864419
tuannguyenminh040294 <37282910+tuannguyenminh040294@users.noreply.github.com>
2020-08-07 01:18:04 +0700

    config api gateway client

--- src/main/java/com/arslan/apigatewayclient/ApiGatewayClientApplication.java
+++ src/main/java/com/arslan/apigatewayclient/ApiGatewayClientApplication.java
@@ -4,9 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ApiGatewayClientApplication {
+    @RequestMapping(value = "/available")
+    public String available() {
+
+        return "Spring in Action";
+
+    }
+
+    @RequestMapping(value = "/checked-out")
+    public String checkedOut() {
+
+        return "Spring Boot in Action";
+
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayClientApplication.class, args);
